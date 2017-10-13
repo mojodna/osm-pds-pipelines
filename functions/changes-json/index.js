6e86afb5fb70bd6a731c57496ef96d31aad48f4a
Marc Farra <marcfarra@gmail.com>
2017-10-13 00:18:46 -0400

    Add changes-json and changesets-json

---
+++ functions/changes-json/index.js
@@ -0,0 +1,33 @@
+require("babel-polyfill");
+
+const {
+  BinarySplitter,
+  sinks: { Kinesis },
+  sources: { Changes }
+} = require("osm-replication-streams");
+const osm2obj = require('osm2obj');
+const stringify = require("stringify-stream");
+
+let checkpointStream = require('./lib/checkpoint-stream');
+
+exports.handle = (event, context, callback) => {
+  checkpointStream(Changes, (err, stream) => {
+    if (err) {
+      console.warn(err.stack);
+      return callback(err);
+    }
+
+    stream
+      .pipe(osm2obj())
+      .pipe(stringify())
+      .pipe(new Kinesis("changes-json"));
+  });
+}
+
+if (require.main === module) {
+  exports.handle({}, {}, (err, body) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
