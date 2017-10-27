8ccb900c5d2dc01a7b3cc8ea612e19c5f041d426
Marc Farra <marcfarra@gmail.com>
2017-10-27 18:45:12 -0400

    Simplify changes protocol by omitting the action attribute and adding a visible attribute.

--- functions/changes/index.js
+++ functions/changes/index.js
@@ -8,10 +8,47 @@ const {
 } = require("osm-replication-streams");
 const osm2obj = require('osm2obj');
 const stringify = require("stringify-stream");
+const through2 = require("through2");
 
 const checkpointStream = require('./lib/checkpoint-stream');
 
 const STREAM_NAME = env.require("STREAM_NAME");
+
+/**
+ * A simplified protocol of the osm2obj format omits
+ * the action property of each OSM entity and attaches
+ * the visible property following these rules:
+ * 
+ * 1. If the action is a create, then visible=true because
+ * a created entity is a visible entity with version=1
+ * 2. If the action is a modify, then visible=true with a 
+ * version > 1
+ * 3. If the action is a delete, then visible=false
+ * 
+ * @param {Object} obj - An osm2obj object
+ * @returns {Object} A simplified object
+ * 
+ */
+function obj2simple (obj) {
+  let res = {};
+
+  // Omit the action
+  for (var prop in obj) {
+    if (prop !== 'action') {
+      res[prop] = obj[prop];
+    }
+  }
+
+  if (obj['action']) {
+    if (obj['action'] === 'create' || obj['action'] === 'modify') {
+      res['visible'] = 'true';
+    }
+    else {
+      res['visible'] = 'false';
+    }
+  }
+  return res;
+}
 
 exports.handle = (event, context, callback) =>
   checkpointStream(Changes, (err, stream) => {
@@ -22,6 +59,11 @@ exports.handle = (event, context, callback) =>
 
     stream
       .pipe(osm2obj())
+      .pipe(through2.obj(function (obj, enc, cb) {
+        const res = obj2simple(obj);
+        this.push(res);
+        cb();
+      }))
       .pipe(stringify())
       .pipe(new Kinesis(STREAM_NAME));
   });
