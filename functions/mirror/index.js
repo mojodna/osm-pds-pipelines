6bc8fd2d4067aea36d6f2227c3a23e6e1ec7c503
Seth Fitzsimmons <sethfitz@amazon.com>
2023-07-25 20:57:19 -0700

    Format with prettier

--- functions/mirror/index.js
+++ functions/mirror/index.js
@@ -1,320 +1,367 @@
-"use strict"
+"use strict";
 
-const path = require('path')
+const path = require("path");
 
-const async = require('async')
-const AWS = require('aws-sdk')
-const env = require('require-env')
-const request = require('request')
-const Rsync = require('rsync')
+const async = require("async");
+const AWS = require("aws-sdk");
+const env = require("require-env");
+const request = require("request");
+const Rsync = require("rsync");
 
-const batch = new AWS.Batch()
-const s3 = new AWS.S3()
+const batch = new AWS.Batch();
+const s3 = new AWS.S3();
 
-const BATCH_JOB_QUEUE = env.require('BATCH_JOB_QUEUE')
-const S3_BUCKET = env.require('S3_BUCKET')
-const RSYNC_SOURCE_PREFIX = 'rsync://planet.osm.org/planet/'
-const HTTP_SOURCE_PREFIX = 'https://planet.osm.org/'
+const BATCH_JOB_QUEUE = env.require("BATCH_JOB_QUEUE");
+const S3_BUCKET = env.require("S3_BUCKET");
+const RSYNC_SOURCE_PREFIX = "rsync://planet.osm.org/planet/";
+const HTTP_SOURCE_PREFIX = "https://planet.osm.org/";
 
 const NOW = new Date();
 
 const PATHS_TO_CHECK = [
-  'planet/' + NOW.getFullYear() - 1 + '/',
-  'planet/' + NOW.getFullYear() + '/',
-  'pbf/',
-  'pbf/full-history/'
-]
+  "planet/" + NOW.getFullYear() - 1 + "/",
+  "planet/" + NOW.getFullYear() + "/",
+  "pbf/",
+  "pbf/full-history/",
+];
 
-const FILES_TO_MIRROR = /((planet|history)-\d{6}.osm.pbf|changesets-\d{6}.osm.bz2)/
-const STARTING_DATE = new Date(Date.now() - (30 * 86400e3))
+const FILES_TO_MIRROR =
+  /((planet|history)-\d{6}.osm.pbf|changesets-\d{6}.osm.bz2)/;
+const STARTING_DATE = new Date(Date.now() - 30 * 86400e3);
 
-const flatten = arr => arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), [])
+const flatten = (arr) =>
+  arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
 const mirror = (src, dst, jobName, dependsOn, callback) => {
-  console.log(`Mirroring ${src} to ${dst}...`)
+  console.log(`Mirroring ${src} to ${dst}...`);
   if (dependsOn != null) {
     dependsOn = [
       {
-        jobId: dependsOn
-      }
-    ]
+        jobId: dependsOn,
+      },
+    ];
   }
 
-  return batch.submitJob({
-    jobDefinition: 'mirror',
-    jobName,
-    jobQueue: BATCH_JOB_QUEUE,
-    parameters: {
-      src,
-      dst
+  return batch.submitJob(
+    {
+      jobDefinition: "mirror",
+      jobName,
+      jobQueue: BATCH_JOB_QUEUE,
+      parameters: {
+        src,
+        dst,
+      },
+      dependsOn,
     },
-    dependsOn
-  }, (err, data) => {
-    if (err) {
-      return callback(err)
+    (err, data) => {
+      if (err) {
+        return callback(err);
+      }
+
+      return callback(null, data.jobId);
     }
+  );
+};
 
-    return callback(null, data.jobId)
-  })
-}
-
-const isTranscodable = filename => 
+const isTranscodable = (filename) =>
   filename.match(/^((planet|history)-\d{6}.osm.pbf|changesets-\d{6}.osm.bz2)$/);
 
 const transcode = (type, src, dst, jobName, dependsOn, callback) => {
-  console.log(`Transcoding (${type}) ${src} to ${dst}...`)
+  console.log(`Transcoding (${type}) ${src} to ${dst}...`);
   if (dependsOn != null) {
     dependsOn = [
       {
-        jobId: dependsOn
-      }
-    ]
+        jobId: dependsOn,
+      },
+    ];
   }
 
-  return batch.submitJob({
-    jobDefinition: `transcode-${type}`,
-    jobName,
-    jobQueue: BATCH_JOB_QUEUE,
-    parameters: {
-      src,
-      dst
+  return batch.submitJob(
+    {
+      jobDefinition: `transcode-${type}`,
+      jobName,
+      jobQueue: BATCH_JOB_QUEUE,
+      parameters: {
+        src,
+        dst,
+      },
+      dependsOn,
     },
-    dependsOn
-  }, (err, data) => {
-    if (err) {
-      return callback(err)
-    }
+    (err, data) => {
+      if (err) {
+        return callback(err);
+      }
 
-    return callback(null, data.jobId)
-  })
-}
+      return callback(null, data.jobId);
+    }
+  );
+};
 
 exports.handle = (event, context, callback) => {
-  console.log('processing event: %j', event)
+  console.log("processing event: %j", event);
 
-  return async.parallel({
-    remote: done => {
-      return async.map(PATHS_TO_CHECK, (path, fin) => {
-        const _stdout = []
-        const _stderr = []
+  return async.parallel(
+    {
+      remote: (done) => {
+        return async.map(
+          PATHS_TO_CHECK,
+          (path, fin) => {
+            const _stdout = [];
+            const _stderr = [];
 
-        const rsync = new Rsync()
-          .set('list-only')
-          .source(`${RSYNC_SOURCE_PREFIX}${path}`)
+            const rsync = new Rsync()
+              .set("list-only")
+              .source(`${RSYNC_SOURCE_PREFIX}${path}`);
 
-        const env = process.env
-        env.PATH += ':/var/task/bin'
+            const env = process.env;
+            env.PATH += "/var/task/bin";
 
-        rsync.env(env)
+            rsync.env(env);
 
-        return rsync.execute((err, code, cmd) => {
-          const stdout = Buffer.concat(_stdout).toString()
-          const stderr = Buffer.concat(_stderr).toString()
+            return rsync.execute(
+              (err, code, cmd) => {
+                const stdout = Buffer.concat(_stdout).toString();
+                const stderr = Buffer.concat(_stderr).toString();
 
-          if (err) {
-            if (code === 23) {
-              // no such path
-              return fin(null, [])
+                if (err) {
+                  if (code === 23) {
+                    // no such path
+                    return fin(null, []);
+                  }
+
+                  console.warn("stdout:\n", stdout);
+                  console.warn("stderr:\n", stderr);
+
+                  return fin(err);
+                }
+
+                const entries = stdout
+                  .split("\n")
+                  .filter((line) => !!line)
+                  .map((line) => {
+                    const parts = line.split(/\s+/);
+
+                    return {
+                      mode: parts[0],
+                      size: Number(parts[1]),
+                      date: new Date(`${parts[2]} ${parts[3]}`),
+                      path: `${path}${parts[4]}`,
+                      filename: parts[4],
+                    };
+                  });
+
+                return fin(null, entries);
+              },
+              (chunk) => {
+                _stdout.push(chunk);
+              },
+              (chunk) => {
+                _stderr.push(chunk);
+              }
+            );
+          },
+          (err, data) => {
+            return done(err, flatten(data));
+          }
+        );
+      },
+      local: (done) => {
+        // TODO when this exceeds 1000 results, things get weird and we start mirroring things that
+        // already exist
+        return s3.listObjectsV2(
+          {
+            Bucket: S3_BUCKET,
+          },
+          (err, data) => {
+            if (err) {
+              return done(err);
             }
 
-            console.warn('stdout:\n', stdout)
-            console.warn('stderr:\n', stderr)
-
-            return fin(err)
-          }
-
-          const entries = stdout.split('\n')
-            .filter(line => !!line)
-            .map(line => {
-              const parts = line.split(/\s+/)
-
+            const entries = data.Contents.map((entry) => {
               return {
-                mode: parts[0],
-                size: Number(parts[1]),
-                date: new Date(`${parts[2]} ${parts[3]}`),
-                path: `${path}${parts[4]}`,
-                filename: parts[4]
-              }
-            })
+                date: entry.LastModified,
+                size: entry.Size,
+                filename: entry.Key,
+              };
+            });
 
-          return fin(null, entries)
-        }, chunk => {
-          _stdout.push(chunk)
-        }, chunk => {
-          _stderr.push(chunk)
-        })
-      }, (err, data) => {
-        return done(err, flatten(data))
-      })
+            return done(null, entries);
+          }
+        );
+      },
     },
-    local: done => {
-      // TODO when this exceeds 1000 results, things get weird and we start mirroring things that
-      // already exist
-      return s3.listObjectsV2({
-        Bucket: S3_BUCKET,
-      }, (err, data) => {
-        if (err) {
-          return done(err)
-        }
+    (err, results) => {
+      if (err) {
+        return callback(err);
+      }
 
-        const entries = data.Contents.map(entry => {
-          return {
-            date: entry.LastModified,
-            size: entry.Size,
-            filename: entry.Key
-          }
-        })
+      const localFiles = results.local.map((info) => info.filename);
+      const toMirror = results.remote
+        .filter((info) => info.filename.match(FILES_TO_MIRROR))
+        .filter((info) => info.date >= STARTING_DATE)
+        .filter((info) => {
+          // filenames don't include the century (and the year may not be in the
+          // path, depending on the source)
+          const ts = info.filename.match(/\d{6}/)[0];
+          const year = Math.floor(NOW.getFullYear() / 100) + ts.slice(0, 2);
 
-        return done(null, entries)
-      })
-    }
-  }, (err, results) => {
-    if (err) {
-      return callback(err)
-    }
+          return localFiles.indexOf(`${year}/${info.filename}`) < 0;
+        });
 
-    const localFiles = results.local.map(info => info.filename)
-    const toMirror = results.remote
-      .filter(info => info.filename.match(FILES_TO_MIRROR))
-      .filter(info => info.date >= STARTING_DATE)
-      .filter(info => {
-        // filenames don't include the century (and the year may not be in the
-        // path, depending on the source)
-        const ts = info.filename.match(/\d{6}/)[0];
-        const year = Math.floor(NOW.getFullYear() / 100) + ts.slice(0, 2)
+      const latestByType = toMirror
+        .map((x) => x.filename)
+        .concat(localFiles)
+        .filter((x) => !x.endsWith(".md5"))
+        .reduce((latest, filename) => {
+          // use replace not path.basename w/ path.extname because multiple extensions may be present
+          const [type, date] = filename
+            .split("/")
+            .pop()
+            .replace(/\..+/, "")
+            .split(/-/);
 
-        return localFiles.indexOf(`${year}/${info.filename}`) < 0
-      })
-
-    const latestByType = toMirror.map(x => x.filename)
-      .concat(localFiles)
-      .filter(x => !x.endsWith('.md5'))
-      .reduce((latest, filename) => {
-        // use replace not path.basename w/ path.extname because multiple extensions may be present
-        const [type, date] = filename.split("/").pop().replace(/\..+/, '').split(/-/)
-
-        if (!Number.isNaN(parseInt(date))) {
-          latest[type] = Math.max(latest[type] || 0, parseInt(date))
-        }
-
-        return latest;
-      }, {})
-
-    return async.forEachLimit(toMirror, 10, (info, done) => {
-      // filenames don't include the century (and the year may not be in the
-      // path, depending on the source)
-      const ts = info.filename.match(/\d{6}/)[0];
-      const year = Math.floor(NOW.getFullYear() / 100) + ts.slice(0, 2)
-
-      if (path.extname(info.filename) !== '.md5' &&
-          localFiles.indexOf(`${year}/${info.filename}.md5`) < 0) {
-        return request.get(`${HTTP_SOURCE_PREFIX}${info.path}.md5`, (err, rsp, body) => {
-          if (err) {
-            return done(err)
+          if (!Number.isNaN(parseInt(date))) {
+            latest[type] = Math.max(latest[type] || 0, parseInt(date));
           }
 
-          return async.parallel({
-            putObject: async.apply(s3.putObject.bind(s3), {
-              Bucket: S3_BUCKET,
-              Key: `${year}/${info.filename}.md5`,
-              Body: body
-            }),
-            submitJob: done => {
-              // use replace not path.basename w/ path.extname because multiple extensions may be present
-              const basename = info.filename.replace(/\..+/, '')
-              const extension = info.filename.split('.').slice(1).join('.');
-              const [type, date] = basename.split(/-/)
-              let target = type
+          return latest;
+        }, {});
 
-              if (type === 'history') {
-                target = 'planet-history'
-              }
+      return async.forEachLimit(
+        toMirror,
+        10,
+        (info, done) => {
+          // filenames don't include the century (and the year may not be in the
+          // path, depending on the source)
+          const ts = info.filename.match(/\d{6}/)[0];
+          const year = Math.floor(NOW.getFullYear() / 100) + ts.slice(0, 2);
 
-              const jobs = [
-                async.apply(
-                  mirror,
-                  `${HTTP_SOURCE_PREFIX}${info.path}`,
-                  `s3://${S3_BUCKET}/${year}/${info.filename}`,
-                  `mirror-${basename}-${extension.split('.').pop()}`,
-                  null
-                )
-              ];
-
-              // compare date to the max date available to determine whether it needs to be placed
-              if ((extension.endsWith('.pbf') || extension.endsWith('.osm.bz2')) && latestByType[type] === Number(date)) {
-                jobs.push(
-                  async.apply(
-                    mirror,
-                    `s3://${S3_BUCKET}/${year}/${info.filename}`,
-                    `s3://${S3_BUCKET}/${type}-latest.${extension}`,
-                    `place-${type}-latest`,
-                  )
-                );
-              }
-
-              if (isTranscodable(info.filename)) {
-                // only transcode PBFs and changesets
-                jobs.push(
-                  async.apply(
-                    transcode,
-                    type,
-                    `s3://${S3_BUCKET}/${year}/${info.filename}`,
-                    `s3://${S3_BUCKET}/${year}/${basename}.orc`,
-                    `transcode-${basename}`
-                  )
-                )
-              }
-
-              return async.waterfall(jobs, (err, jobId) => {
+          if (
+            path.extname(info.filename) !== ".md5" &&
+            localFiles.indexOf(`${year}/${info.filename}.md5`) < 0
+          ) {
+            return request.get(
+              `${HTTP_SOURCE_PREFIX}${info.path}.md5`,
+              (err, rsp, body) => {
                 if (err) {
                   return done(err);
                 }
 
-                // compare date to the max date available to determine whether it needs to be placed
-                if (isTranscodable(info.filename) && latestByType[type] === Number(date)) {
-                  return mirror(
-                    `s3://${S3_BUCKET}/${year}/${basename}.orc`,
-                    `s3://${S3_BUCKET}/${target}/${type}-latest.orc`,
-                    `place-${basename}`,
-                    jobId,
-                    done
-                  )
-                }
+                return async.parallel(
+                  {
+                    putObject: async.apply(s3.putObject.bind(s3), {
+                      Bucket: S3_BUCKET,
+                      Key: `${year}/${info.filename}.md5`,
+                      Body: body,
+                    }),
+                    submitJob: (done) => {
+                      // use replace not path.basename w/ path.extname because multiple extensions may be present
+                      const basename = info.filename.replace(/\..+/, "");
+                      const extension = info.filename
+                        .split(".")
+                        .slice(1)
+                        .join(".");
+                      const [type, date] = basename.split(/-/);
+                      let target = type;
 
-                return done()
-              })
-            }
-          }, done)
-        })
-      }
+                      if (type === "history") {
+                        target = "planet-history";
+                      }
 
-      return done()
-    }, err => {
-      if (err) {
-        return callback(err)
-      }
+                      const jobs = [
+                        async.apply(
+                          mirror,
+                          `${HTTP_SOURCE_PREFIX}${info.path}`,
+                          `s3://${S3_BUCKET}/${year}/${info.filename}`,
+                          `mirror-${basename}-${extension.split(".").pop()}`,
+                          null
+                        ),
+                      ];
 
-      return callback()
-    })
+                      // compare date to the max date available to determine whether it needs to be placed
+                      if (
+                        (extension.endsWith(".pbf") ||
+                          extension.endsWith(".osm.bz2")) &&
+                        latestByType[type] === Number(date)
+                      ) {
+                        jobs.push(
+                          async.apply(
+                            mirror,
+                            `s3://${S3_BUCKET}/${year}/${info.filename}`,
+                            `s3://${S3_BUCKET}/${type}-latest.${extension}`,
+                            `place-${type}-latest`
+                          )
+                        );
+                      }
 
-    // TODO compare toMirror with results.local (using filename)
+                      if (isTranscodable(info.filename)) {
+                        // only transcode PBFs and changesets
+                        jobs.push(
+                          async.apply(
+                            transcode,
+                            type,
+                            `s3://${S3_BUCKET}/${year}/${info.filename}`,
+                            `s3://${S3_BUCKET}/${year}/${basename}.orc`,
+                            `transcode-${basename}`
+                          )
+                        );
+                      }
 
-    // TODO keep status (or copy MD5s in this process to determine which jobs to queue)
-  })
-}
+                      return async.waterfall(jobs, (err, jobId) => {
+                        if (err) {
+                          return done(err);
+                        }
+
+                        // compare date to the max date available to determine whether it needs to be placed
+                        if (
+                          isTranscodable(info.filename) &&
+                          latestByType[type] === Number(date)
+                        ) {
+                          return mirror(
+                            `s3://${S3_BUCKET}/${year}/${basename}.orc`,
+                            `s3://${S3_BUCKET}/${target}/${type}-latest.orc`,
+                            `place-${basename}`,
+                            jobId,
+                            done
+                          );
+                        }
+
+                        return done();
+                      });
+                    },
+                  },
+                  done
+                );
+              }
+            );
+          }
+
+          return done();
+        },
+        (err) => {
+          if (err) {
+            return callback(err);
+          }
+
+          return callback();
+        }
+      );
+
+      // TODO compare toMirror with results.local (using filename)
+
+      // TODO keep status (or copy MD5s in this process to determine which jobs to queue)
+    }
+  );
+};
 
 if (require.main === module) {
-  exports.handle({
-
-  }, {
-
-  }, (err, body) => {
+  exports.handle({}, {}, (err, body) => {
     if (err) {
-      throw err
+      throw err;
     }
 
     if (body) {
-      console.log(body)
+      console.log(body);
     }
-  })
+  });
 }
